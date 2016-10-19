520c768c168650973c8241603d1e4fb505007b39
alu0100763478 <alu0100763478@ull.edu.es>
2016-10-19 10:45:50 +0100

    Angular js para mostrar datos de registro

---
+++ js/datosInicio.js
@@ -0,0 +1,29 @@
+var misDatos = angular.module("datosApp", []);
+
+misDatos.controller('ControladordeUsuarios', ['$scope', function($scope) {
+  $scope.usuario = [{}];
+
+
+  $scope.registrar = function() {
+    $scope.usuario.push( {email: $scope.usuario.email, nombre: $scope.usuario.nombre, apellido: $scope.usuario.apellido});
+  };
+
+}]);
+
+
+
+
+/*misDatos.controller('controladorDatos', function( $scope, $http){
+
+    $scope.importar = function(){
+
+      $http.get('coches.json').success(function(datos){
+
+          $scope.equipo = datos;
+
+      });
+    }
+
+  $scope.importar();
+
+});*/
